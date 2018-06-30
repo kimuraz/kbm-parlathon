238538c8a46a1bd1969fac492a27a616f461a61f
Kimura <joseduardo.kimura@gmail.com>
2018-06-30 14:29:32 -0300

    :bulb: Add title

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import { Typography } from '@material-ui/core';
 import './index.css';
 
 import Search from './components/Search';
@@ -10,6 +11,9 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Typography color="primary" style={{ marginTop: "10px", textAlign: "center"}} variant="headline">
+          Trilha Legislativa!
+        </Typography>
         <BrowserRouter>
           <Switch>
             <Route exact path='/' component={Search} />
