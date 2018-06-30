8e422857694be838b2d0ca8536493ecb6089f7a4
Marcelo Cristiano <marceloabk@hotmail.com>
2018-06-29 22:10:44 -0300

    Creating requests

--- src/models/Proposition.js
+++ src/models/Proposition.js
@@ -1,10 +1,28 @@
 class Proposition {
+  /*
+  * Colocar aqui as principais informações que precisarmos da proposição.
+  * O intuito dessa class é mostrar, de forma padronizada,
+  * as proposições vindas das duas diferentes APIs.
+  */
+  constructor(numeroCasaIniciadora, anoCasaIniciadora, numeroCasaRevisadora, anoCasaRevisadora, autores,
+              ementa, explicacaoEmenta, formaTramitacao, regimeTramitacao, dataHoraTramitacao, orgaoTramitacao,
+              acaoTramitacao, situacaoAtual, tempoCorridoDias) {
+    this.numeroCasaIniciadora = numeroCasaIniciadora
+    this.anoCasaIniciadora = anoCasaIniciadora
+    this.numeroCasaRevisadora = numeroCasaRevisadora
+    this.anoCasaRevisadora = anoCasaRevisadora
+    this.autores = autores
+    this.ementa = ementa
+    this.explicacaoEmenta = explicacaoEmenta
+    this.formaTramitacao = formaTramitacao
+    this.regimeTramitacao = regimeTramitacao
+    this.dataHoraTramitacao = dataHoraTramitacao
+    this.orgaoTramitacao = orgaoTramitacao
+    this.acaoTramitacao = acaoTramitacao
     /*
-    * Colocar aqui as principais informações que precisarmos da proposição.
-    * O intuito dessa class é mostrar, de forma padronizada, 
-    * as proposições vindas das duas diferentes APIs.
-    */
-    constructor() {
-
-    }
+     * TODO: Verificar se existem mais dados de tramitação
+     */
+    this.situacaoAtual = situacaoAtual
+    this.tempoCorridoDias = tempoCorridoDias
+  }
 }
--- src/utils/Api.js
+++ src/utils/Api.js
@@ -1,3 +1,5 @@
+import Proposition from '../models/Proposition'
+
 const camaraURL = 'https://dadosabertos.camara.leg.br/api/v2/proposicoes?formato=json&siglaTipo=PL';
 const senadoURL = '';
 
@@ -6,3 +8,104 @@ export default {
         return fetch(`${camaraURL}&keywords=${query}`).then(res => res.json());
     },
 }
+
+function getDataFromPL(plDados, autores, orgaoTramitacao, tempoCorridoDias) {
+  plDados = plDados['dados']
+
+  let numeroCasaIniciadora = String(plDados.numero)
+  let anoCasaIniciadora = String(plDados.ano)
+
+  let numeroCasaRevisadora = undefined
+  let anoCasaRevisadora = undefined
+  if (plDados.uriPropPosterior) {
+    let urlCasaRevisadora = plDados.uriPropPosterior
+    urlCasaRevisadora = urlCasaRevisadora.split('/')
+    // const prefixo = urlCasaRevisadora[urlCasaRevisadora.length - 3]
+    const numero = urlCasaRevisadora[urlCasaRevisadora.length - 2]
+    const ano = urlCasaRevisadora[urlCasaRevisadora.length - 1]
+
+    numeroCasaRevisadora = numero
+    anoCasaRevisadora = ano
+  } else {
+
+  }
+  let ementa = plDados.ementa
+  let explicacaoEmenta = undefined
+  if (plDados.ementaDetalhada) {
+    explicacaoEmenta = plDados.ementaDetalhada
+  }
+  let formaTramitacao = undefined
+  /*
+   * TODO: tramitação conclusiva (não vai a Plenário). Não sei identificar esse dado
+   */
+  let regimeTramitacao = plDados.statusProposicao.regime
+  let dataHoraTramitacao = plDados.statusProposicao.dataHora
+  let acaoTramitacao = undefined
+  /*
+   * TODO: NÃO ENTENDI O QUE É
+   */
+  let situacaoAtual = plDados.statusProposicao.descricaoSituacao
+  return new Proposition(numeroCasaIniciadora, anoCasaIniciadora, numeroCasaRevisadora, anoCasaRevisadora, autores,
+    ementa, explicacaoEmenta, formaTramitacao, regimeTramitacao, dataHoraTramitacao, orgaoTramitacao,
+    acaoTramitacao, situacaoAtual, tempoCorridoDias)
+}
+
+function getAuthorsFromPL(autoresAPI) {
+  let autores = []
+  autoresAPI = autoresAPI['dados']
+  for (let i = 0; i < autoresAPI.length; i++) {
+    autores.push(`${autoresAPI[i].tipo}(a) ${autoresAPI[i].nome}`)
+  }
+  return autores
+}
+
+function getTimeSinceBegin(tramitacoes) {
+  let first = new Date(tramitacoes.dados[0].dataHora)
+  let last = new Date(tramitacoes.dados[tramitacoes.dados.length - 1].dataHora)
+  let difference = undefined
+  first > last ? difference = first - last: difference = last - first;
+
+  // milliseconds to days rounded down
+  return Math.floor(difference/86400000)
+}
+
+function getProcessingFromPL(orgaoTramitacao) {
+  orgaoTramitacao = orgaoTramitacao['dados']
+  return orgaoTramitacao.nome
+}
+
+app.get('/', function (req, response) {
+  let law = req.query.law
+  if (law) {
+    law = law.split('-')
+
+    let autoresAPI = undefined
+    let plDados = undefined
+    let orgaoTramitacao = undefined
+    let tramitacoes = undefined
+    fetch(`https://dadosabertos.camara.leg.br/api/v2/proposicoes?formato=json&numero=${law[0]}&ano=${law[1]}`)
+    .then(res => res.json())
+    .then(json => fetch(`${json.dados[0].uri}?formato=json`))
+    .then(res => res.json())
+    .then(json => plDados = json)
+    .then(json => fetch(`${json['dados'].uriAutores}?formato=json`))
+    .then(res => res.json())
+    .then(json => autoresAPI = json)
+    .then(() => fetch(`${plDados['dados'].statusProposicao.uriOrgao}?formato=json`))
+    .then(res => res.json())
+    .then(json => orgaoTramitacao = json)
+    .then(() => fetch(`https://dadosabertos.camara.leg.br/api/v2/proposicoes/${plDados['dados'].id}/tramitacoes?formato=json`))
+    .then(res => res.json())
+    .then(json => tramitacoes = json)
+    .then(() => {
+      let autores = getAuthorsFromPL(autoresAPI)
+      let orgao = getProcessingFromPL(orgaoTramitacao)
+      let tempoCorridoDias = getTimeSinceBegin(tramitacoes)
+      let proposition = getDataFromPL(plDados, autores, orgao, tempoCorridoDias)
+
+      response.send(proposition)
+    })
+  } else {
+    console.log('deu ruim')
+  }
+});
